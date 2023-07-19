0d07a4805f8b9f6a967ebd92fd3f90d1b3b63fea
Marius Bjørnstad <pmb@fa2k.net>
2023-07-19 12:23:53 +0200

    add full docker dl commented

--- download-pipeline-script.sh
+++ download-pipeline-script.sh
@@ -2,7 +2,13 @@
 
 # Alternative: Use docker image
 # curl https://raw.githubusercontent.com/fa2k/dockerfiles/main/nfcore-singularity/Dockerfile | docker build -t nfcore -
-# docker run -ti --rm -v $PWD:$PWD -w $PWD nfcore nf-core download -r dev -c singularity raredisease
+# docker run -ti --rm -v $PWD:$PWD -w $PWD -e NXF_SINGULARITY_CACHEDIR=$PWD/singularity \
+#	nfcore \
+#	nf-core download \
+#	    -c singularity \
+#	    --compress none \
+#	    raredisease \
+#	    -r dev
 
 # Run in conda environment nf-core
 #conda activate nf-core
