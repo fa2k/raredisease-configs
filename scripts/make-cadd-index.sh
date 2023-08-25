28ac86f9dd31d148e47af7f1de9a832d69b246fe
Marius Bjørnstad <pmb@fa2k.net>
2023-08-25 17:11:50 +0000

    tabix: setting bin size may not work

--- scripts/make-cadd-index.sh
+++ scripts/make-cadd-index.sh
@@ -4,4 +4,5 @@ CADD="/refData/CADD-v1.6"
 
 singularity exec -B $(realpath ../refData):/refData \
     ../singularity/depot.galaxyproject.org-singularity-bcftools-1.17--haef29d1_0.img \
-            tabix --csi -b 2 -e 2 -m 9 $CADD/whole_genome_SNVs.tsv.gz
+            tabix --csi -b 2 -e 2 $CADD/whole_genome_SNVs.tsv.gz
+
